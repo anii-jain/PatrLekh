932e53eccc0055a291782860e2d4621bcb3c2b3a
Animesh Jain <ajanimesh20026@gmail.com>
2023-03-21 01:09:49 +0530

    connecting the database

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose  = require('mongoose');
+const mongoURI = "mongodb://localhost:27017"
+
+const connectToMongo = ()=> {
+    mongoose.connect(mongoURI)
+        .then(() => console.log('connected to mongo'))
+        .catch((err) => console.log('Connection failed'));
+}
+
+module.exports = connectToMongo;
